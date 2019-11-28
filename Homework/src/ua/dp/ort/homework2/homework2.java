98564187ed56c1309e8a9e671a4df61805a0a4bb
Slava <Slava@192.168.0.101>
2019-11-28 21:17:39 +0200

    Add my homework 2

---
+++ Homework/src/ua/dp/ort/homework2/homework2.java
@@ -0,0 +1,73 @@
+package ua.dp.ort.homework2;
+
+public class homework2 {
+	public static final int AREA = 45;
+	public static final int WIDTH = 1020;
+	public static final double VOLUME = 8.5;
+	public static final int LENGTH = 840;
+	public static final int PERIMETR = 267;
+
+	public static void main(String[] args) {
+		System.out.println(AREA);
+		System.out.println(WIDTH);
+		System.out.println(VOLUME);
+		System.out.println(LENGTH);
+		System.out.println(PERIMETR);
+		
+		byte petyaYears = 23;
+		System.out.println(petyaYears);
+		byte rabbits = 40;
+		System.out.println(rabbits);
+		byte quantityApples = 5;
+		System.out.println(quantityApples);
+		
+		short raspberries = 1000;
+		System.out.println(raspberries);
+		short cars = 23420;
+		System.out.println(cars);
+		short elephantWeight = 10323;
+		System.out.println(elephantWeight);
+		
+		int areaDnipro = 415000000;
+		System.out.println(areaDnipro);
+		int peopleUkraine = 48000000;
+		System.out.println(peopleUkraine);
+		int  matchesBox = 36 ; 
+		System.out.println(matchesBox);
+		
+		long peopleWorld = 700000000;
+		System.out.println(peopleWorld);
+		long trees = 21504450;
+		System.out.println(trees);
+		long pears =500450;
+		System.out.println(pears);
+		
+		float frogWeight = 25.5f;
+		System.out.println(frogWeight);
+		float halfThree = 1.5f;
+		System.out.println(halfThree);
+		float tables = 139.2f;
+		System.out.println(tables);
+		
+		double averageScore = 23456.2;
+		System.out.println(averageScore);
+		double lengthRoad = 50322.9;
+		System.out.println(lengthRoad);
+		double papers = 459952.5;
+		System.out.println(papers);
+		
+		char symbol = 'T';
+		System.out.println(symbol);
+		char symbolAsteriks = '*';
+		System.out.println(symbolAsteriks);
+		char symbolLess = '<';
+		System.out.println(symbolLess);
+		
+//		У нас есть 7 пациентов то есть 7 бит.   
+//		8-й бит это состояние пациента. 1 - на осмотре, 0 - на лечении
+//		1000 0111 - 1,2,3 пациенты на осмотре
+//		0100 1000 - 4,7 пациенты на лечении 
+//		1111 1111 - все пациенты на осмотре
+	}
+
+}
