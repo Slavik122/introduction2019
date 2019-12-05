f7db5d935c20132c0a361bb9793bc81838953664
Slava <Slava@192.168.0.102>
2019-12-05 22:03:03 +0200

    add homework4

---
+++ Homework/src/ua/dp/ort/homework3/homework3.java
@@ -0,0 +1,69 @@
+package ua.dp.ort.homework3;
+
+public class homework3 {
+
+	public static void main(String[] args) {
+		
+//		1)
+//		int a = 5;
+//		int b = 10;
+//		System.out.println(b + a);
+//		System.out.println(b - a);
+//		System.out.println(b * a);
+//		System.out.println(b / a);
+//		2)
+//		double a1 = 5;
+//		double a2 = 5;
+//		System.out.println(Math.pow(a1, a2));
+//		3)
+//		int x = 2;
+//		int y = 2 * x + 5;
+//		System.out.println(y);
+//		double x1 = 9.5;
+//		double y1 = 2 * x1 + 5;
+//		System.out.println(y1);
+//		4)
+//		double x = 4.5;
+//		double y = 3.1;
+//		System.out.println(x * y + " m^2");
+//		System.out.println(Math.floor(x * y) + " m^2");
+//		5)
+//		double apples = 11;
+//		double children = 3;
+//		double appleChild = Math.floor(apples / children);
+//		double applesChildren = appleChild * 3;
+//		double applesLeave = apples -  applesChildren;
+//		System.out.println("every child has:  " + appleChild + "  apples");
+//		System.out.println("all children have:  " + applesChildren + "  apples"); 
+//		System.out.println("leave:  " + applesLeave + "  apples");
+//		6)
+//		double distance= 143;
+//		double frogJump = 2.5;
+//		double totalJumps = (distance / frogJump);
+//		System.out.println(Math.floor(totalJumps));
+//		7)
+//		int a = 1999999999;
+//		int b = 1999999999;
+//		int c = a + b;
+//		System.out.println(Math.rint(c));
+//		8)
+//		int numeral = 435679;
+//		int a = numeral / 100000;
+//		int b = numeral / 10000 % 10;
+//		int c = numeral / 1000 % 10;
+//		int d = numeral / 100 % 10;
+//		int e = numeral / 10 % 10;
+//		int f = numeral / 1 % 10;
+//		int sum = a + b + c + d + e + f;
+//		System.out.println(sum);		
+//		9)
+//		int a = 0b0000_1111_1111;
+//		int b = 0b0000_0000_0000;
+//		System.out.println(a & b);
+//		int a1 = 0b0000_1111_1111;
+//		int b1 = 0b0000_0000_0001;
+//		System.out.println(a1 & b1);
+
+	}
+
+}
