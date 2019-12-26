caf4cc2384daf815c0869099d0951f1d83a75683
Slava <Slava@192.168.0.101>
2019-12-26 14:25:19 +0200

    changes

--- Homework/src/ua/dp/ort/homework5/homework5.java
+++ Homework/src/ua/dp/ort/homework5/homework5.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class homework5 {
 
 	public static final int COFFEE = 1;
-	public static final int PRICECOF = 5;
+	public static final int PRICE_COF = 5;
 	public static final int WATER = 2;
-	public static final int PRICEWAT = 3;
+	public static final int PRICE_WAT = 3;
 	public static final int COLA = 3;
-	public static final int PRICECOL = 10;
+	public static final int PRICE_COL = 10;
 	public static final int PEPSI = 4;
-	public static final int PRICEPEP = 10;
+	public static final int PRICE_PEP = 10;
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -33,39 +33,43 @@ public class homework5 {
 		String message = null;
 		int balance = 0;
 		if (choise == COFFEE) {
-			if (money < PRICECOF) {
-				System.out.println("You don't have enough money");
-				return;
+			balance = money - PRICE_COF;
+			if (money >= PRICE_COF) {
+				System.out.println("You have bought coffee." + "\n" + "It costs 5 ");
+				message = "You have left " + balance;
+			} else {
+				if (money < PRICE_COF)
+					message = "You don't have enough money";
 			}
-			System.out.println("You have bought coffee." + "\n" + "It costs 5 ");
-			balance = money - PRICECOF;
-			message = "You have left ";
 		} else if (choise == WATER) {
-			if (money < PRICEWAT) {
-				System.out.println("You don't have enough money");
-				return;
+			balance = money - PRICE_WAT;
+			if (money >= PRICE_WAT) {
+				System.out.println("You have bought sparkling water." + "\n" + "It costs 3 ");
+				message = "You have left " + balance;
+			} else {
+				if (money < PRICE_WAT)
+					message = "You don't have enough money";
 			}
-			System.out.println("You have bought sparkling water." + "\n" + "It costs 3 ");
-			balance = money - PRICEWAT;
-			message = "You have left money: ";
 		} else if (choise == COLA) {
-			if (money < PRICECOL) {
-				System.out.println("You don't have enough money");
-				return;
+			balance = money - PRICE_COL;
+			if (money >= PRICE_COL) {
+				System.out.println("You have bought cola." + "\n" + "It costs 10 ");
+				message = "You have left " + balance;
+			} else {
+				if (money < PRICE_COL)
+					message = "You don't have enough money";
 			}
-			System.out.println("You have bought cola." + "\n" + "It costs 10 ");
-			balance = money - PRICECOL;
-			message = "You have left: ";
 		} else if (choise == PEPSI) {
-			if (money < PRICEPEP) {
-				System.out.println("You don't have enough money");
-				return;
+			balance = money - PRICE_PEP;
+			if (money >= PRICE_PEP) {
+				System.out.println("You have bought pepsi." + "\n" + "It costs 10 ");
+				message = "You have left " + balance;
+			} else {
+				if (money < PRICE_PEP)
+					message = "You don't have enough money";
 			}
-			System.out.println("You have bought pepsi." + "\n" + "It costs 10 ");
-			balance = money - PRICEPEP;
-			message = "You have left: ";
 		}
-		System.out.println(message + balance);
+		System.out.println(message);
 	}
 
 }
