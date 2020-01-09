8e16884bdce6643426c3b33b24f14d4e85b4d626
Slava <Slava@192.168.0.101>
2020-01-09 16:03:42 +0200

    add homework 6

--- Homework/src/ua/dp/ort/homework6/homework6.java
+++ Homework/src/ua/dp/ort/homework6/homework6.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class homework6 {
 
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 		double allVolume = 0.0;
 		double portions = 0.0;
