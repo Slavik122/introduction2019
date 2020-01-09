a62c4f3437026d0f601408cda61fb76924324742
Slava <Slava@192.168.0.101>
2020-01-09 21:14:12 +0200

    changed

--- Homework/src/ua/dp/ort/Homework_6/Homework_6.java
+++ Homework/src/ua/dp/ort/Homework_6/Homework_6.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Homework_6 {
 
+	public static final int VOLUME_OF_TANK = 1000;
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -12,29 +14,29 @@ public class Homework_6 {
 		double clientVolume = 0.0;
 
 		double volume = 0, fuel = 0, clientVolumeFuel = 0;
-		
-		boolean Boolean = true;
 
-		while (Boolean) {
+		boolean booleanTrueFalse = true;
+
+		while (booleanTrueFalse) {
 			System.out.print("Input overall volume: ");
 			String overallVolume = sc.nextLine();
 			volume = Double.parseDouble(overallVolume);
 
-			if (volume < 1000) {
+			if (volume < VOLUME_OF_TANK) {
 				System.out.println("it's less than minimum");
-				Boolean = false;
+				booleanTrueFalse = false;
 				break;
 			} else {
 				allVolume = volume;
 				break;
 			}
 		}
-		while (Boolean) {
+		while (booleanTrueFalse) {
 			System.out.print("Input how many milliliters in one portion of fuel: ");
 			String fuelPortions = sc.nextLine();
 			fuel = Double.parseDouble(fuelPortions);
 			fuel = fuel / 1000;
-			
+
 			if (fuel < 0) {
 				System.out.println("it's can not be negative number");
 			} else {
@@ -42,11 +44,11 @@ public class Homework_6 {
 				break;
 			}
 		}
-		while (Boolean) {
+		while (booleanTrueFalse) {
 			System.out.print("Input your volume: ");
 			String clientFuel = sc.nextLine();
 			clientVolumeFuel = Double.parseDouble(clientFuel);
-			
+
 			if ((clientVolumeFuel < 0) || (clientVolumeFuel > volume)) {
 				System.out.println("it's can not be negative number or bigger than minimum");
 			} else {
