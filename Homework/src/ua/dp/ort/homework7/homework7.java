583adcf5ff46d8bd614bceaeec782a0ee9ac3799
Slava <Slava@192.168.0.103>
2020-01-24 19:57:49 +0200

    changed

--- Homework/src/ua/dp/ort/homework7/homework7.java
+++ Homework/src/ua/dp/ort/homework7/homework7.java
@@ -14,28 +14,29 @@ public class homework7 {
 		int sumEvenNumbers = 0;
 		int average = 0;
 		int amountEvenNumbers = 0;
+		int counter;
 
 //		1)
 
-		for (int counterOfUserInput = 0; counterOfUserInput < userArray.length; counterOfUserInput++) {
-			System.out.print("Number for array[" + counterOfUserInput + "] -> ");
+		for (counter = 0; counter < userArray.length;counter++) {
+			System.out.print("Number for array[" + counter + "] -> ");
 			userInput = scanner.nextLine();
 			int numberForArray = Integer.parseInt(userInput);
 
-			userArray[counterOfUserInput] = numberForArray;
+			userArray[counter] = numberForArray;
 		}
 
 //		2)
 
-		for (int positiveNumberFinder = 0; positiveNumberFinder < userArray.length; positiveNumberFinder++) {
+		for (counter = 0; counter < userArray.length; counter++) {
 
-			if ((userArray[positiveNumberFinder] % 2 == 0) && (userArray[positiveNumberFinder] > 0)) {
-				sumEvenNumbers = sumEvenNumbers + userArray[positiveNumberFinder];
+			if ((userArray[counter] % 2 == 0) && (userArray[counter] > 0)) {
+				sumEvenNumbers = sumEvenNumbers + userArray[counter];
 			}
 
-			if (userArray[positiveNumberFinder] > 0) {
+			if (userArray[counter] > 0) {
 				amountEvenNumbers++;
-				average = average + userArray[positiveNumberFinder];
+				average = average + userArray[counter];
 			}
 
 		}
@@ -45,12 +46,12 @@ public class homework7 {
 		int min_1 = 999999;
 		int min_2 = 999999;
 
-		for (int counter = 0; counter < userArray.length; counter++) {
+		for (counter = 0; counter < userArray.length; counter++) {
 			if (min_1 > userArray[counter]) {
 				min_1 = userArray[counter];
 			}
 		}
-		for (int counter = 0; counter < userArray.length; counter++) {
+		for (counter = 0; counter < userArray.length; counter++) {
 			if (userArray[counter] > min_1 && userArray[counter] < min_2) {
 				min_2 = userArray[counter];
 			}
@@ -58,76 +59,80 @@ public class homework7 {
 
 //		4)
 
-//		int minimalNumber = userArray[0];
-//
-//		for (int counter = 1; counter < userArray.length; counter++) {
-//			if (minimalNumber > userArray[counter]) {
-//				minimalNumber = userArray[counter];
-//			}
-//		}
-//
-//		int maximalNumber = userArray[0];
-//
-//		for (int counter_1 = 1; counter_1 < userArray.length; counter_1++) {
-//			if (maximalNumber < userArray[counter_1]) {
-//				maximalNumber = userArray[counter_1];
-//			}
-//		}
-			
-			
+		int minimalNumber = userArray[0];
+		int minimalCounter = 0;
+
+		for (counter = 1; counter < userArray.length; counter++) {
+			if (minimalNumber > userArray[counter]) {
+				minimalNumber = userArray[counter];
+				minimalCounter++;
+			}
+		}
+
+		int maximalNumber = userArray[0];
+		int maximalCounter = 0;
+
+		for (counter = 1; counter < userArray.length; counter++) {
+			if (maximalNumber < userArray[counter]) {
+				maximalNumber = userArray[counter];
+				maximalCounter++;
+			}
+		}
+		
+		
 
 //		5)
 
-			int sizeArray = userArray.length;
+		int sizeArray = userArray.length;
 
-			for (int counter_2 = 0; counter_2 < sizeArray / 2; counter_2++) {
-				int tempArray = userArray[counter_2];
-				userArray[counter_2] = userArray[sizeArray - 1 - counter_2];
-				userArray[sizeArray - 1 - counter_2] = tempArray;
-			}
+		for (counter = 0; counter < sizeArray / 2; counter++) {
+			int tempArray = userArray[counter];
+			userArray[counter] = userArray[sizeArray - 1 - counter];
+			userArray[sizeArray - 1 - counter] = tempArray;
+		}
 
 //		6)
 
-			int counterNegativeNumbers = 0;
+		int counterNegativeNumbers = 0;
 
-			for (int counter = 0; counter < userArray.length; counter++) {
-				if (userArray[counter] < 0) {
-					counterNegativeNumbers++;
-				}
+		for (counter = 0; counter < userArray.length; counter++) {
+			if (userArray[counter] < 0) {
+				counterNegativeNumbers++;
 			}
-
-			int[] negativeNumbersArray = new int[counterNegativeNumbers];
-			int index_1 = 0;
-
-			for (int counter = 0; counter < userArray.length; counter++) {
-				if (userArray[counter] < 0) {
-					negativeNumbersArray[index_1] = userArray[counter];
-					index_1++;
-				}
-			}
-
-			int counterPositiveNumbers = 0;
-			for (int counter = 0; counter < userArray.length; counter++) {
-				if (userArray[counter] > 0) {
-					counterPositiveNumbers++;
-				}
-			}
-
-			int[] positiveNumbersArray = new int[counterPositiveNumbers];
-			int index_2 = 0;
-
-			for (int counter = 0; counter < userArray.length; counter++) {
-				if (userArray[counter] > 0) {
-					positiveNumbersArray[index_2] = userArray[counter];
-					index_2++;
-				}
-			}
-
-			System.out.println("Sum of all even numbers is -> " + sumEvenNumbers);
-			System.out.println("Average of all positive numbers is -> " + average / amountEvenNumbers);
-			System.out.println("Minimum number is -> " + min_1 + ", " + min_2);
-			System.out.println("Array after reverse: " + Arrays.toString(userArray));
-			System.out.println("All negative numbers from first array " + Arrays.toString(negativeNumbersArray));
-			System.out.println("All positive numbers from first array " + Arrays.toString(positiveNumbersArray));
 		}
+
+		int[] negativeNumbersArray = new int[counterNegativeNumbers];
+		int index_1 = 0;
+
+		for (counter = 0; counter < userArray.length; counter++) {
+			if (userArray[counter] < 0) {
+				negativeNumbersArray[index_1] = userArray[counter];
+				index_1++;
+			}
+		}
+
+		int counterPositiveNumbers = 0;
+		for (counter = 0; counter < userArray.length; counter++) {
+			if (userArray[counter] > 0) {
+				counterPositiveNumbers++;
+			}
+		}
+
+		int[] positiveNumbersArray = new int[counterPositiveNumbers];
+		int index_2 = 0;
+
+		for (counter = 0; counter < userArray.length; counter++) {
+			if (userArray[counter] > 0) {
+				positiveNumbersArray[index_2] = userArray[counter];
+				index_2++;
+			}
+		}
+
+		System.out.println("Sum of all even numbers is -> " + sumEvenNumbers);
+		System.out.println("Average of all positive numbers is -> " + average / amountEvenNumbers);
+		System.out.println("Minimum number is -> " + min_1 + ", " + min_2);
+		System.out.println("Array after reverse: " + Arrays.toString(userArray));
+		System.out.println("All negative numbers from first array " + Arrays.toString(negativeNumbersArray));
+		System.out.println("All positive numbers from first array " + Arrays.toString(positiveNumbersArray));
 	}
+}
